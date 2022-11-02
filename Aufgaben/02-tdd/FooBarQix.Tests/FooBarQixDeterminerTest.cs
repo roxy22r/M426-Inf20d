a6331c181699130a71c98c66944792f26374bbba
raksana ravichandran <raksana.ravichandran@loewenfels.ch>
2022-11-02 09:23:20 +0100

    Added Dertermine method logic and diffrent Testcases with ouput number as text or  text

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -4,9 +4,65 @@ namespace FooBarQix.Tests
 {
     public class FooBarQixDeterminerTest
     {
-        [Fact]
-        public void Test1()
+      
+       [Fact]
+        public void Determine_DivideByThree_ReturnFoo()
         {
+            //arrange
+            FooBarQixDeterminer testee = new FooBarQixDeterminer();
+            //act
+            string actual = testee.Determine(3);
+            //assert
+            Assert.Equal("Foo", actual);
         }
+        [Fact]
+        public void Determine_DivideByThree_ReturnBar()
+        {
+            //arrange
+            FooBarQixDeterminer testee = new FooBarQixDeterminer();
+            //act
+            string actual = testee.Determine(5);
+            //assert
+            Assert.Equal("Bar", actual);
+        }
+        [Fact]
+        public void Determine_DivideByThree_ReturnQix()
+        {
+            //arrange
+            FooBarQixDeterminer testee = new FooBarQixDeterminer();
+            //act
+            string actual = testee.Determine(7);
+            //assert
+            Assert.Equal("Qix", actual);
+        }
+        [Theory]
+
+        [InlineData(15, "FooBar")]
+        [InlineData(21, "FooQix")]
+        [InlineData(105, "FooBarQix")]
+        public void Determine_DivideByThree_ReturnWordCombination(int number, string expected)
+        {
+            //arrange
+            FooBarQixDeterminer testee = new FooBarQixDeterminer();
+            //act
+            string actual = testee.Determine(number);
+            //assert
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(32)]
+        public void Determine_DivideByThree_ReturnNumber(int number)
+        {
+            //arrange
+            FooBarQixDeterminer testee = new FooBarQixDeterminer();
+            //act
+            string actual = testee.Determine(number);
+            //assert
+            Assert.Equal(number.ToString(), actual);
+        }
+
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,34 @@
 {
     public class FooBarQixDeterminer
     {
+        public string Determine(int input)
+        {
+            string result = "";
+            if (input % 3 == 0)
+            {
+                result = result + "Foo";
+            }
+             if (input % 5 == 0)
+            {
+                result = result + "Bar";
+            }
+             if (input % 7 == 0)
+            {
+                result = result + "Qix";
+            }
+            return isEmptyReturnNumberElseText(input, result);
+        }
 
+        private static string isEmptyReturnNumberElseText(int input, string result)
+        {
+            if (string.IsNullOrEmpty(result))
+            {
+                return input.ToString();
+            }
+            else
+            {
+                return result;
+            }
+        }
     }
 }
