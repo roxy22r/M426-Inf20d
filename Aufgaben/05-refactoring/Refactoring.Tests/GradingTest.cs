42070a9eb135b45e6aa240dad6f2ac9c2530f83a
roxy22 <Raksana_Ravichandran@sluz.ch>
2022-12-19 16:59:41 +0100

    Changed grading to a compact calculation

--- Aufgaben/05-refactoring/Refactoring.Tests/GradingTest.cs
+++ Aufgaben/05-refactoring/Refactoring.Tests/GradingTest.cs
@@ -1,6 +1,77 @@
+using System;
+using Xunit;
 namespace Refactoring.Tests;
 
 public class GradingTest 
 {
-    // TODO
+    [Fact]
+    public void getLetterGrade_gradeOne_A()
+    {
+        // Arrange
+        // Act
+       char result = Grading.GetLetterGrade(1);
+
+        // Assert
+        Assert.Equal('A',result);
+    }
+    [Fact]
+    public void getLetterGrade_gradeTwo_B()
+    {
+        // Arrange
+        // Act
+        char result = Grading.GetLetterGrade(2);
+
+        // Assert
+        Assert.Equal('B', result);
+    }
+    public void getLetterGrade_gradeThree_C()
+    {
+        // Arrange
+        // Act
+        char result = Grading.GetLetterGrade(3);
+
+        // Assert
+        Assert.Equal('C', result);
+    }
+    public void getLetterGrade_gradeFour_D()
+    {
+        // Arrange
+        // Act
+        char result = Grading.GetLetterGrade(4);
+
+        // Assert
+        Assert.Equal('D', result);
+    }
+    public void getLetterGrade_gradeFive_E()
+    {
+        // Arrange
+        // Act
+        char result = Grading.GetLetterGrade(5);
+
+        // Assert
+        Assert.Equal('E', result);
+    }
+    public void getLetterGrade_gradeSix_F()
+    {
+        // Arrange
+        // Act
+        char result = Grading.GetLetterGrade(6);
+        // Assert
+        Assert.Equal('F', result);
+    }
+
+    public void getLetterGrade_gradeHigherThenSix_thorwArgumentException()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Throws<ArgumentException>(() => Grading.GetLetterGrade(7));
+    }
+    public void getLetterGrade_gradeLowerThanOne_thorwArgumentException()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Throws<ArgumentException>(() =>Grading.GetLetterGrade(0));
+    }
 }
--- Aufgaben/05-refactoring/Refactoring/Grading.cs
+++ Aufgaben/05-refactoring/Refactoring/Grading.cs
@@ -4,22 +4,9 @@ public class Grading
 {
     public static char GetLetterGrade(int grade)
     {
-        switch (grade)
-        {
-            case 1:
-                return 'A';
-            case 2:
-                return 'B';
-            case 3:
-                return 'C';
-            case 4:
-                return 'D';
-            case 5:
-                return 'E';
-            case 6:
-                return 'F';
-            default:
-                throw new ArgumentException("unknown grade " + grade);
-        }
+        return grade >= 1 && grade <= 6 ?//
+               (char)(grade + 64) : throw new ArgumentException("unknown grade " + grade);
+
+     
     }
 }
